c0b1c208fe34db8d173591ec3fbb326923fd4be3
donny-dynamite <mumbodumbo4@gmail.com>
2026-03-20 19:03:13 +0800

    Update 01_Open_Process_Handle.py
    refactor
    - function signature(s)
    - func() def wrapper for OpenProcess()
    - better error handling

--- 01_Open_Process_Handle.py
+++ 01_Open_Process_Handle.py
@@ -1,7 +1,12 @@
+"""
+Open a handle to an existing process, using WinAPI via ctypes
+Calls PowerShell script to group PIDs by ProcessName
+"""
+
 import ctypes
+from ctypes import wintypes
 import subprocess
 
-kernel32 = ctypes.WinDLL('kernel32.dll')
 
 # Run PowerShell script - list PIDs, grouped by ProcessName (raw string is okay here)
 script = r'''
@@ -19,32 +24,49 @@ print(pid_list.stdout)
 # loop for valid integer to be entered
 while True:
     try:
-        pid_value = int(input("Enter a single PID from above list: "))
-        if pid_value:
+        pid_value = int(input("\nEnter a single PID from above list: "))
+        if pid_value > 0:
             break
         else:
-            print(f"Please enter single PID from above list")
+            print(f"\nPlease enter single PID from above list")
     except ValueError as e:
         print(f"\n[!] Invalid Input, Error: {e}")
 
 
 
+#################################
+##### Function Signature(s) #####
+#################################
+kernel32 = ctypes.WinDLL('kernel32.dll')
 
-#########################
-##### OpenProcess() #####
-#########################
+kernel32.OpenProcess.argtypes = [
+    wintypes.DWORD,
+    wintypes.BOOL,
+    wintypes.DWORD
+]
+kernel32.OpenProcess.restype = wintypes.HANDLE
+
+
+# define parameters
+#
 # Ref: https://learn.microsoft.com/en-us/windows/win32/procthread/process-security-and-access-rights
 
-PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xFFFF)
-dwDesiredAccess = PROCESS_ALL_ACCESS
-bInheritHandle = False
-dwProcessId = pid_value
+PROCESS_ALL_ACCESS = 0x1F0FFF
+g_dwDesiredAccess = PROCESS_ALL_ACCESS
+g_bInheritHandle = False
+g_dwProcessId = pid_value
+
+
+# func() def to call OpenProcess()
+def open_proc(dwDesiredAccess, bInheritHandle, dwProcessId):
+    ret = kernel32.OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId)
+    if not ret:
+        raise ctypes.WinError()    
+    return ret
+
 
 try:
-    proc_handle = kernel32.OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId)
-    if proc_handle:
-        print(f"\n[+] OpenProcess() Successful, Process Handle: {proc_handle}")
-    else:
-        raise ctypes.WinError()
-except Exception as e:
-    print(f"\n[!] OpenProcess() Failed. Error Code: {e.winerror}, Message: {e.strerror}")
+    pHandle = open_proc(g_dwDesiredAccess, g_bInheritHandle, g_dwProcessId)
+    print(f"\n[+] OpenProcess() Successful, Process Handle: {pHandle}")
+except OSError as e:
+    print(f"\n[!] OpenProcess() Failed. Error: {e}")
