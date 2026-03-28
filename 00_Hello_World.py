5fb4e91407e350844d806697fa87033a1f273448
donny-dynamite <mumbodumbo4@gmail.com>
2026-03-28 21:57:31 +0800

    Update 00_Hello_World.py
    refactor to cleanup

--- 00_Hello_World.py
+++ 00_Hello_World.py
@@ -1,17 +1,25 @@
 """
 Display Windows MessageBox using WinAPI via ctypes
-func() def to wrap MessageBoxW() API call
 """
 
 import ctypes
 from ctypes import wintypes
 
-
-###############################
-##### Function Signatures #####
-###############################
+# import required DLL
 user32 = ctypes.WinDLL('user32.dll', use_last_error=True)
 
+#########################
+##### MessageBoxW() #####
+#########################
+#
+# - hWnd not required, but explicity defined for completeness
+# - uType, doco defines 4-byte hex value
+#          but can also pass short-form, or corresponding integer
+#          ie 0x00000030 or 0x30 or 48 (decimal)
+#
+# Ref: https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messageboxw
+
+# func() sig 
 user32.MessageBoxW.argtypes = [
     wintypes.HWND,
     wintypes.LPCWSTR,
@@ -20,30 +28,18 @@ user32.MessageBoxW.argtypes = [
 ]    
 user32.MessageBoxW.restype = wintypes.INT
 
-# define parameters
-# ref: https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messageboxw
-#
-# whilst hWnd not necessary, explicity defined as standard
-#
-# for uType, doco defines 4-byte long integer in hex, can also pass short-form, or corresponding integer
-# ie 0x00000030 or 0x30 or 48 (decimal)
-
-global_hWnd = None 
-global_lpText = 'Hello Text'
-global_lpCaption = 'Hello Caption'
-global_uType = 0x00000001 # MB_OKCANCEL
-
-
-# func() def to call MessageBoxW()
-def msg_box(hWnd, lpText, lpCaption, uType):
-    ret = user32.MessageBoxW(hWnd, lpText, lpCaption, uType)
-    if ret == 0:
-        raise ctypes.WinError()
-    return ret
+# def params
+g_hWnd = None 
+g_lpText = 'Hello Text'
+g_lpCaption = 'Hello Caption'
+g_uType = 0x00000001 # MB_OKCANCEL
 
 
 try:
-    ret_code = msg_box(global_hWnd, global_lpText, global_lpCaption, global_uType)
+    ret_code = user32.MessageBoxW(g_hWnd, g_lpText, g_lpCaption, g_uType)
+    if not ret_code:
+        raise ctypes.WinError()
     print(f"\n[+] MessageBoxW() Successful, Response Code: {ret_code}")
+
 except OSError as e:
-    print(f"\n[!] MessageBowX() Failed, Error: {e}")
+    print(f"\n[!] MessageBoxW() Failed, Error: {e}")
