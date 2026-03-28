a3057243b7f758c889223f1ab0aa12e5c18335db
donny-dynamite <mumbodumbo4@gmail.com>
2026-03-28 22:38:17 +0800

    Update 01_Open_Process_Handle.py
    refactor
    - powershell scripts cleanup
    - handle cleanup

--- 01_Open_Process_Handle.py
+++ 01_Open_Process_Handle.py
@@ -1,18 +1,26 @@
 """
 Open a handle to an existing process, using WinAPI via ctypes
-Calls PowerShell script to group PIDs by ProcessName
-
-Note: investigate refactoring with context manager
-- try/except/finally feels messy
-- may be better as with func() as var:
+- utilises PowerShell scripts for PID validation
 """
 
 import ctypes
 from ctypes import wintypes
 import subprocess
+import sys
 
 
-# Run PowerShell script - list PIDs, grouped by ProcessName (raw string is okay here)
+kernel32 = ctypes.WinDLL('kernel32.dll', use_last_error=True)
+
+
+##############################
+##### PowerShell scripts #####
+##############################
+#
+# - list PIDs, grouped by ProcessName (raw string is okay here)
+# - loop for a valid integer to be entered
+# - validate entered integer exists as a PID in tasklist/Get-Process
+
+# list PIDs, grouped by ProcessName (raw string is okay here)
 script = r'''
 Get-Process | Group-Object ProcessName | % {
     [PSCustomObject]@{
@@ -22,27 +30,67 @@ Get-Process | Group-Object ProcessName | % {
 }
 '''
 
-pid_list = subprocess.run(["powershell", "-command", script], capture_output=True, text=True)
+pid_list = subprocess.run(["powershell", "-NoProfile", "-Command", script], capture_output=True, text=True)
 print(pid_list.stdout)
+
 
 # loop for valid integer to be entered
 while True:
     try:
-        pid_value = int(input("\nEnter a single PID from above list: "))
+        pid_value = int(input("\nPlease enter a valid PID: "))
         if pid_value > 0:
             break
         else:
-            print(f"\nPlease enter single PID from above list")
+            print(f"\nPlease enter a valid PID: ")
     except ValueError as e:
         print(f"\n[!] Invalid Input, Error: {e}")
 
 
+# validate entered PID
+valid_pid = subprocess.run(["powershell", "-NoProfile", "-Command", f"Get-Process -ID {pid_value}"],
+                            capture_output=True, text=True)
 
-#################################
-##### Function Signature(s) #####
-#################################
-kernel32 = ctypes.WinDLL('kernel32.dll', use_last_error=True)
+if not valid_pid.stdout.strip():
+    sys.exit(f"[!] PID {pid_value} non-existant. Exiting")
 
+
+
+
+#########################
+##### CloseHandle() #####
+#########################
+#
+# for clean-up of open handles at the end of execution
+# 
+# Ref: https://learn.microsoft.com/en-us/windows/win32/api/handleapi/nf-handleapi-closehandle#return-value
+
+# func() sig
+kernel32.CloseHandle.argtypes = [wintypes.HANDLE]
+kernel32.CloseHandle.restype = wintypes.BOOL
+
+# wrapper for CloseHandle()
+def close_handle(handle, name="Handle"):
+    if handle:
+        if kernel32.CloseHandle(handle):
+            print(f"[+] CloseHandle() Successful, {name} Handle: {handle.value}")
+            
+        else:
+            print(f"[!] CloseHandle() Failed, {name}: {handle.value}, Error: {ctypes.get_last_error()}")
+
+
+
+
+#########################
+##### OpenProcess() #####
+#########################
+#
+# Return HANDLE to open process
+# - force return type of HANDLE in func() wrapper, otherwise ctypes converts to type 'int'
+# - return wintypes.HANDLE(ret)
+#
+# Ref: https://learn.microsoft.com/en-us/windows/win32/procthread/process-security-and-access-rights
+
+# func() sig
 kernel32.OpenProcess.argtypes = [
     wintypes.DWORD,
     wintypes.BOOL,
@@ -50,39 +98,34 @@ kernel32.OpenProcess.argtypes = [
 ]
 kernel32.OpenProcess.restype = wintypes.HANDLE
 
-kernel32.CloseHandle.argtypes = [wintypes.HANDLE]
-kernel32.CloseHandle.restype = wintypes.BOOL
-
-
-# define parameters
-# Ref: https://learn.microsoft.com/en-us/windows/win32/procthread/process-security-and-access-rights
-
-PROCESS_ALL_ACCESS = 0x1F0FFF
+# def params
+PROCESS_ALL_ACCESS = 0x1F0FFF # likely to fail unless run elevated/as-admin
+# PROCESS_QUERY_LIMITED_INFORMATION = 0x1000
 g_dwDesiredAccess = PROCESS_ALL_ACCESS
 g_bInheritHandle = False
 g_dwProcessId = pid_value
 
 
-# func() def to call OpenProcess()
+# wrapper for OpenProcess()
 def open_proc(dwDesiredAccess, bInheritHandle, dwProcessId):
     ret = kernel32.OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId)
     if not ret:
         raise ctypes.WinError()    
-    return ret
+    return wintypes.HANDLE(ret)
 
-pHandle = None
+
+# so that finally: works in case try:/except: fails
+proc_handle = None 
+
 
 try:
-    pHandle = open_proc(g_dwDesiredAccess, g_bInheritHandle, g_dwProcessId)
-    print(f"\n[+] OpenProcess() Successful, Process Handle: {pHandle}")
-except OSError as e:
+    proc_handle = open_proc(g_dwDesiredAccess, g_bInheritHandle, g_dwProcessId)
+    print(f"\n[+] OpenProcess() Successful, Process Handle: {proc_handle.value}")
+
+except Exception as e:
     print(f"\n[!] OpenProcess() Failed. Error: {e}")
+
 finally:
-    if pHandle not in (None, 0):
-        handle_close = kernel32.CloseHandle(pHandle)
-        if handle_close:
-            print(f"\n[+] CloseHandle() Successful, Handle: {pHandle}")
-            pHandle = None
-        else:
-            error = kernel32.GetLastError()
-            print(f"[!] CloseHandle() Failed, Error: {error}")
+    if proc_handle and proc_handle.value:
+        print("\n[!] Closing opened handles:\n---------------------------")
+        close_handle(proc_handle, "Process")
