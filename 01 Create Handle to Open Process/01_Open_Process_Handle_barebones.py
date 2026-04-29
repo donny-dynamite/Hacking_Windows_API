799f925dce8f3d8cffc1e4c2cd50be20e65bb4a4
donny-dynamite <mumbodumbo4@gmail.com>
2026-04-30 01:59:36 +0800

    Update 01_Open_Process_Handle_barebones.py

--- 01 Create Handle to Open Process/01_Open_Process_Handle_barebones.py
+++ 01 Create Handle to Open Process/01_Open_Process_Handle_barebones.py
@@ -1,14 +1,11 @@
-"""
-Get handle to a/current process
-"""
+""" Get handle to a/current process """
 import ctypes
 
 kernel32 = ctypes.WinDLL('kernel32.dll')
-PROCESS_ALL_ACCESS = 0x1F0FFF
 
 # method 1 (or other non-current based on pid)
 pid = kernel32.GetCurrentProcessId()
-pHandle = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
+pHandle = kernel32.OpenProcess(0x1F0FFF, False, pid)
 
 # method 2 - pseudo-handle
 pHandle = kernel32.GetCurrentProcess()
